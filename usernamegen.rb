829577d5c87272045504e375dec75e8412789fb2
Adam Vaughan <adam_vaughan1982@hotmail.com>
2020-03-06 18:56:50 +0000

    ruby code that puts 2 adjectives and a profession together to create a random username

---
+++ usernamegen.rb
@@ -0,0 +1,41 @@
+def pick_random_adj
+  chosen_line = nil
+  File.open './adjectives.txt' do |file|
+    file.each_with_index do |adj1, index|
+      chosen_line = adj1 if rand < 1.0/(index+1)
+    end
+    return chosen_line.chomp
+  end
+end
+
+def pick_random_profession
+  chosen_line = nil
+  File.open './profession.txt' do |file|
+    file.each_with_index do |profession, index|
+      chosen_line = profession if rand < 1.0/(index+1)
+    end
+    return chosen_line.chomp
+  end
+end
+
+
+class Username
+  @adj1 = []
+  @adj2 = []
+  @profession = []
+
+  def initialize(adj1, adj2, profession)
+    @adj1 = adj1.capitalize
+    @adj2 = adj2.capitalize
+    @profession = profession
+  end
+
+  def print_username
+    puts "\nYour username is #{@adj1}#{@adj2}#{@profession}"
+  end
+
+end
+
+game = Username.new(pick_random_adj, pick_random_adj, pick_random_profession)
+
+puts game.print_username
